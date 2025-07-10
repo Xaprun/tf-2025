b411d9a33d4e29608486d6f3869ea6d67ce5741c
Xaprun <106160173+Xaprun@users.noreply.github.com>
2025-07-10 23:24:44 +0200

    Update main.tf[skip ci]

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,62 +1,5 @@
-
-resource "azurerm_resource_group" "example" {
-  name     = "rg-vm-tf-test"
-  location = "westeurope"
-}
-
-resource "azurerm_virtual_network" "example" {
-  name                = "vnet-vm-tf"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-}
-
-resource "azurerm_subnet" "example" {
-  name                 = "subnet-tf-01"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_network_interface" "example" {
-  name                = "nic-tf-01"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_windows_virtual_machine" "example" {
-  name                = "vm-01"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  size                = "Standard_B1s"
-  admin_username      = "azureuser"
-  admin_password      = var.vm_admin_password
-  network_interface_ids = [
-    azurerm_network_interface.example.id,
-  ]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2019-Datacenter"
-    version   = "latest"
-  }
-}
-
-
 ###############################
-######## TFMAESTRO VRS ########
+##### TFMAESTRO MODIFIED ######
 ###############################
 resource "azurerm_resource_group" "rg" {
   name     = "${var.name}-rg"
@@ -70,13 +13,5 @@ module "vnet" {
   location            = azurerm_resource_group.rg.location
   address_space       = var.address_space
   resource_group_name = azurerm_resource_group.rg.name
-
-  subnets = {
-    "dev-subnet-01" = {
-      address_prefix = "10.0.3.0/24"
-    }
-    "dev-subnet-02" = {
-      address_prefix = "10.0.4.0/24"
-    }
-  }
+  subnets             = var.subnets
 }
