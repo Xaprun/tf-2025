e1e1d8347f1c5b502459dfa279b8a8f9afb5aff5
Xaprun <106160173+Xaprun@users.noreply.github.com>
2025-07-10 13:41:07 +0200

    Update main.tf provider[skip ci]

--- infra/bootstrap-backend/main.tf
+++ infra/bootstrap-backend/main.tf
@@ -2,11 +2,9 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "4.1.0"
     }
   }
-
-  backend "local" {}
 }
 
 provider "azurerm" {
