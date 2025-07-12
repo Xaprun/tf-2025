151e36b06ba6cd876f63c1b30a6385c06fc90e19
Xaprun <106160173+Xaprun@users.noreply.github.com>
2025-07-12 05:37:58 +0200

    Update main.tf

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -9,7 +9,7 @@ resource "azurerm_resource_group" "rg" {
 module "vnet" {
   # source              = "../../modules/vnet/"
   source = "git::https://github.com/xaprun/tfmodules.git//modules/vnet?ref=vnet-v1.0.0"
-  name                = var.name
+  name                = var.network_name
   location            = azurerm_resource_group.rg.location
   address_space       = var.address_space
   resource_group_name = azurerm_resource_group.rg.name
