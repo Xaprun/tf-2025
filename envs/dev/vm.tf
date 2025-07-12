5667ebaf3d9b7b207f0a002f9969171ef9587d8e
Xaprun <106160173+Xaprun@users.noreply.github.com>
2025-07-12 05:01:06 +0200

    Create vm.tf

---
+++ envs/dev/vm.tf
@@ -0,0 +1,25 @@
+module "vm" {
+  source = "git::https://github.com/Xaprun/tfmodules.git//modules/vm?ref=main"
+
+  resource_group_name     = "${var.environment}-rg"
+  resource_group_location = azurerm_resource_group.main.location
+  network_name            = var.location
+  environment             = var.environment
+  network_name            = var.network_name
+  admin_username          = "azureuser"
+  admin_ssh_key_path      = "ssh/admin_key.pub"
+  custom_data_file        = "cloud-init/init.sh"
+
+  vm_config = {
+    "vm1" = {
+      private_ip          = "10.0.1.10"
+      public_ip_name      = "vm1-public-ip"
+      machine_type        = "Standard_B1ms"
+      machine_description = "Test VM 1"
+    }
+  }
+  
+}
+
+
+    
