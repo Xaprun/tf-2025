60d5409e577c8d3448942b6feee99f2fcf5a0427
Xaprun <106160173+Xaprun@users.noreply.github.com>
2025-07-12 05:21:39 +0200

    Create init.sh

---
+++ envs/dev/cloud-init/init.sh
@@ -0,0 +1,5 @@
+              #!/bin/bash
+              # Aktualizacja pakietów
+              apt-get update -y
+              # Instalacja narzędzi do analizy sieci (net-tools)
+              # apt-get install -y net-tools
