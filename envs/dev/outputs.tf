a38a818f1e8f6100eb8ddb91cb9ee618c055cb0e
zbook <xaprun@git>
2025-07-13 21:24:40 +0200

    update

--- envs/dev/outputs.tf
+++ envs/dev/outputs.tf
@@ -1,13 +1,5 @@
 output "subnet_ids" {
   value = { for s in azurerm_subnet.subnet : s.name => s.id }
 }
-output "private_ips" {
-  description = "Private IP addresses of the created virtual machines"
-  value       = module.vm.private_ips
-}
-output "public_ips" {
-  description = "Public IP addresses of the created virtual machines"
-  value       = module.vm.public_ips
-}
 
 
