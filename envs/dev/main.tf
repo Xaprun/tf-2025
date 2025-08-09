7973425263b0f3eb4b9c793f733ca682b53b8120
Xaprun <106160173+Xaprun@users.noreply.github.com>
2025-08-09 17:22:16 +0200

    Update main.tf

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -41,17 +41,38 @@ module "vm" {
     }
   }
   firewall_rules = {
-      Allow-SSH = {
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "22"
-        # source_address_prefix      = var.admin_source_cidr
-        source_address_prefix      = "*"
-        destination_address_prefix = "*"
-        priority                   = 100
-    }
   }
 
+}
+##################
+##################
+# Allow SSH only from your IP/CIDR (replace!)
+variable "admin_source_cidr" {
+  type        = string
+  description = "CIDR allowed to SSH (e.g., 1.2.3.4/32)"
+  default = "*"
+}
+
+resource "azurerm_network_security_group" "ssh" {
+  name                = "${var.name}-${var.location}-ssh-nsg"
+  location            = azurerm_resource_group.rg01.location
+  resource_group_name = azurerm_resource_group.rg01.name
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = var.admin_source_cidr  # e.g. "1.2.3.4/32"
+    destination_address_prefix = "*"
+  }
+}
+
+# Attach NSG to the subnet your VM uses
+resource "azurerm_subnet_network_security_group_association" "subnet06" {
+  subnet_id                 = module.vnet-rg01-01.subnet_ids["subnet-06"]
+  network_security_group_id = azurerm_network_security_group.ssh.id
 }
