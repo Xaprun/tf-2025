374ff2699c064f28fb216977c8ff3c0137a51193
Xaprun <106160173+Xaprun@users.noreply.github.com>
2025-08-09 22:51:59 +0200

    Update main.tf

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -28,7 +28,8 @@ module "vm" {
   admin_username          = "azureuser"
   public_subnet_name      = keys(var.subnets)[0]
   admin_ssh_key_path      = "ssh/admin_key.pub"
-  custom_data_file      = "cloud-init/init.sh"
+  custom_data_file        = filebase64("/cloud-init/init.sh")
+  # custom_data_b64 = filebase64("${path.module}/cloud-init/init.sh")
   subnet_id               = module.vnet-rg01-01.subnet_ids["subnet-06"]
   depends_on              = [azurerm_resource_group.rg01, module.vnet-rg01-01]
 
