a1636d973aa083cacc38ce29e2b56a3034f2b8ee
Xaprun <106160173+Xaprun@users.noreply.github.com>
2025-08-09 06:36:25 +0200

    Update main.tf

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -12,7 +12,7 @@ module "vnet-rg01-01" {
   name                  = "${var.name}-${var.location}-${var.network_name}"
   location              = azurerm_resource_group.rg01.location
   address_space         = var.address_space
-  resource_group_name   = azurerm_resource_group.rrg.name
+  resource_group_name   = azurerm_resource_group.rg01.name
   subnets               = var.subnets
 }
 
