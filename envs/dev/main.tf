3f495e1c4bb79d1eb2e06846fabba44178f3f921
Xaprun <106160173+Xaprun@users.noreply.github.com>
2025-08-09 10:27:42 +0200

    Update main.tf

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -16,3 +16,29 @@ module "vnet-rg01-01" {
   subnets               = var.subnets
 }
 
+################################
+module "vm" {
+  source = "git::https://github.com/Xaprun/tfmodules.git//modules/vm?ref=main"
+
+  resource_group_name     = azurerm_resource_group.rg01.name
+  resource_group_location = azurerm_resource_group.rg01.location
+  environment             = var.environment
+  network_name            = "${var.name}-${var.location}-${var.network_name}"
+
+  admin_username          = "azureuser"
+  public_subnet_name      = var.public_subnet_name
+  admin_ssh_key_path      = "ssh/admin_key.pub"
+  # custom_data_file      = "cloud-init/init.sh"
+  subnet_id               = module.vnet.subnet_ids["subnet-06"]
+  depends_on              = [azurerm_resource_group.rg01, module.vnet]
+
+  vm_config = {
+    "${var.name}-${var.location}-vm1" = {
+      private_ip          = "10.0.6.10"
+      public_ip_name      = "vm1-public-ip"
+      machine_type        = "Standard_B1ms"
+      machine_description = "Test VM 1"
+    }
+  }
+  firewall_rules = {}
+}
