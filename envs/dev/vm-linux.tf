12670446e4b5849d6fb671eb8556dadb1f350110
Xaprun <106160173+Xaprun@users.noreply.github.com>
2025-08-10 17:50:30 +0200

    Update vm-linux.tf

--- envs/dev/vm-linux.tf
+++ envs/dev/vm-linux.tf
@@ -4,25 +4,25 @@
 module "vm" {
   source = "git::https://github.com/Xaprun/tfmodules.git//modules/vm?ref=main"
 
-  resource_group_name     = azurerm_resource_group.rg01.name
-  resource_group_location = azurerm_resource_group.rg01.location
-  environment             = var.environment
-  network_name            = "${var.name}-${var.location}-${var.network_name}"
+  resource_group_name     = azurerm_resource_group.rg-01.name
+  resource_group_location = var.location
+  # environment             = var.environment
+  # network_name            = "${var.name}-${var.location}-${var.network_name}"
 
   admin_username          = "azureuser"
-  public_subnet_name      = keys(var.subnets)[0]
+  public_subnet_name      = keys(var.subnets)[1]
   admin_ssh_key_path      = "ssh/admin_key.pub"
   custom_data_file        = "cloud-init/init.sh"
-  # custom_data_file        = "${path.root}/cloud-init/init.sh"
-  # custom_data_file        = "${path.module}/cloud-init/init.sh"
-  # custom_data_b64 = filebase64("${path.module}/cloud-init/init.sh")
-  subnet_id               = module.vnet-rg01-01.subnet_ids["subnet-06"]
-  depends_on              = [azurerm_resource_group.rg01, module.vnet-rg01-01]
+  # custom_data_file      = "${path.root}/cloud-init/init.sh"
+  # custom_data_file      = "${path.module}/cloud-init/init.sh"
+  # custom_data_b64       = filebase64("${path.module}/cloud-init/init.sh")
+  subnet_id               = module.vnet-01.subnet_ids["subnet-06"]
+  depends_on              = [azurerm_resource_group.rg-01, module.vnet-01]
 
   vm_config = {
-    "${var.name}-${var.location}-vm1" = {
-      private_ip          = "10.0.6.10"
-      public_ip_name      = "vm1-public-ip"
+    "${var.prefix}-vm-li01" = {
+      # private_ip          = "10.0.6.10"
+      public_ip_name      = "vm-li01-public-ip"
       machine_type        = "Standard_B1ms"
       machine_description = "Test VM 1"
     }
@@ -42,9 +42,9 @@ variable "admin_source_cidr" {
 }
 
 resource "azurerm_network_security_group" "ssh" {
-  name                = "${var.name}-${var.location}-ssh-nsg"
-  location            = azurerm_resource_group.rg01.location
-  resource_group_name = azurerm_resource_group.rg01.name
+  name                = "${var.prefix}-lin-ssh-nsg"
+  location            = azurerm_resource_group.rg-01.location
+  resource_group_name = azurerm_resource_group.rg-01.name
 
   security_rule {
     name                       = "Allow-SSH"
@@ -60,7 +60,7 @@ resource "azurerm_network_security_group" "ssh" {
 }
 
 # Attach NSG to the subnet your VM uses
-resource "azurerm_subnet_network_security_group_association" "subnet06" {
-  subnet_id                 = module.vnet-rg01-01.subnet_ids["subnet-06"]
+resource "azurerm_subnet_network_security_group_association" "subnet-lin" {
+  subnet_id                 = module.vnet-01.subnet_ids["subnet-lin"]
   network_security_group_id = azurerm_network_security_group.ssh.id
 }
