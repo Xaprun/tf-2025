4f03d5fc7bbc1133167c4f1ae663f435cd8d5987
Xaprun <106160173+Xaprun@users.noreply.github.com>
2025-08-10 17:22:26 +0200

    Update main.tf

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,8 +1,37 @@
 ###############################
+########### BACKEND ###########
+###############################
+
+backend_resource_group_name = "tf-backend-rg"
+
+###############################
+########### TFVARS ############
+###############################
+
+# PROJECT/CLIENT SPECYFIC CONFIGURATION:
+name          = "p01"
+location      = "westeurope"
+environment   = "dev"
+prefix        = "${var.name}-${var.environment}-${var.location}"
+
+# NETWORK CONFIG:
+network_name  = "dev"
+address_space = ["10.0.0.0/16"]
+subnets = {
+  "subnet-05" = {
+    address_prefix = "10.0.5.0/24"
+  }
+  "subnet-06" = {
+    address_prefix = "10.0.6.0/24"
+  }
+}
+
+###############################
 ############# RGs #############
 ###############################
+
 resource "azurerm_resource_group" "rg01" {
-  name     = "${var.name}-${var.location}-rg"
+  name     = "${var.name}-${var.environment}-${var.location}-rg"
   location = var.location
 }
 
@@ -14,31 +43,10 @@ module "vnet-rg01-01" {
   # source              = "../../modules/vnet/"
   source                = "git::https://github.com/xaprun/tfmodules.git//modules/vnet?ref=vnet-v1.0.1"
   name                  = "${var.name}-${var.location}-${var.network_name}"
-  location              = azurerm_resource_group.rg01.location
+  location              = var.location
   address_space         = var.address_space
   resource_group_name   = azurerm_resource_group.rg01.name
   subnets               = var.subnets
-
 }
 
-###############################
-########### TFVARS ############
-###############################
-backend_resource_group_name = "tf-backend-rg"
 
-name          = "p01"
-network_name  = "dev"
-location      = "westeurope"
-environment   = "dev"
-address_space = ["10.0.0.0/16"]
-subnets = {
-  "subnet-05" = {
-    address_prefix = "10.0.5.0/24"
-  }
-  "subnet-06" = {
-    address_prefix = "10.0.6.0/24"
-  }
-}
-
-# resource_group_location = "westeurope"
-# public_subnet_name      = "subnet-public"
