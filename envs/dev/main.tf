78ed9927ab45c2c2d257fa51a2341327a94d1648
Xaprun <106160173+Xaprun@users.noreply.github.com>
2025-08-10 17:35:21 +0200

    Update main.tf

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -9,20 +9,21 @@ backend_resource_group_name = "tf-backend-rg"
 ###############################
 
 # PROJECT/CLIENT SPECYFIC CONFIGURATION:
-name          = "p01"
-location      = "westeurope"
-environment   = "dev"
-prefix        = "${var.name}-${var.environment}-${var.location}"
+name           = "p01"
+location       = "westeurope"
+location_short = "we"
+environment    = "dev"
+prefix         = "${var.name}-${var.environment}-${var.location_short}"
 
 # NETWORK CONFIG:
-network_name  = "dev"
+network_name  = "${var.prefix}-vnet"
 address_space = ["10.0.0.0/16"]
 subnets = {
-  "subnet-05" = {
-    address_prefix = "10.0.5.0/24"
+  "subnet-win" = {
+    address_prefix = "10.0.10.0/24"
   }
-  "subnet-06" = {
-    address_prefix = "10.0.6.0/24"
+  "subnet-lin" = {
+    address_prefix = "10.0.20.0/24"
   }
 }
 
@@ -30,8 +31,8 @@ subnets = {
 ############# RGs #############
 ###############################
 
-resource "azurerm_resource_group" "rg01" {
-  name     = "${var.name}-${var.environment}-${var.location}-rg"
+resource "azurerm_resource_group" "rg-01" {
+  name     = "${var.prefix}-rg"
   location = var.location
 }
 
@@ -39,13 +40,12 @@ resource "azurerm_resource_group" "rg01" {
 ########### VNETS #############
 ###############################
 
-module "vnet-rg01-01" {
-  # source              = "../../modules/vnet/"
+module "vnet-01" {
   source                = "git::https://github.com/xaprun/tfmodules.git//modules/vnet?ref=vnet-v1.0.1"
-  name                  = "${var.name}-${var.location}-${var.network_name}"
+  name                  = var.network_name
   location              = var.location
   address_space         = var.address_space
-  resource_group_name   = azurerm_resource_group.rg01.name
+  resource_group_name   = azurerm_resource_group.rg-01.name
   subnets               = var.subnets
 }
 
