50dff2dab9bbd22646b2d9a9210c17c7ba7eccca
Xaprun <106160173+Xaprun@users.noreply.github.com>
2025-08-11 06:17:48 +0200

    Update vm-linux.tf

--- envs/dev/vm-linux.tf
+++ envs/dev/vm-linux.tf
@@ -2,75 +2,44 @@
 ###############################
 ########### VM DEBIAN #########
 ###############################
-module "vm-lin-01" {
-  source = "git::https://github.com/Xaprun/tfmodules.git//modules/vm?ref=main"
-
-  resource_group_name     = azurerm_resource_group.rg-01.name
+module "vm_lin_01" {
+  source                  = "git::https://github.com/Xaprun/tfmodules.git//modules/vm?ref=main"
+  resource_group_name     = azurerm_resource_group.rg_01.name
   resource_group_location = var.location
-  # environment           = var.environment
-  network_name            = "${local.prefix}-vnet"
-
+  subnet_id               = module.vnet_01.subnet_ids["subnet-lin"]
   admin_username          = "azureuser"
-  public_subnet_name      = keys(var.subnets)[1]
   admin_ssh_key_path      = "ssh/admin_key.pub"
-  custom_data_file      = "cloud-init/init.sh"
-  # custom_data_file      = "${path.module}/cloud-init/init.sh"
-  # custom_data_file         = filebase64("cloud-init/init.sh")
-  subnet_id               = module.vnet-01.subnet_ids["subnet-lin"]
-  depends_on              = [azurerm_resource_group.rg-01, module.vnet-01]
+  custom_data_file        = "cloud-init/init.sh"
+
+  enable_public_ip = false  # module default
 
   vm_config = {
     "${local.prefix}-vm-li01" = {
-      private_ip          = var.prv_ip_lin_prv
-      public_ip_name      = "vm-li01-public-ip"
       machine_type        = "Standard_B1ms"
-      machine_description = "Test VM 1"
+      machine_description = "public"
+      assign_public_ip    = true   # per-VM override
     }
   }
-  firewall_rules = {
-  }
-
 }
-# Attach NSG to the subnet your VM uses
-resource "azurerm_subnet_network_security_group_association" "subnet-lin" {
-  subnet_id                 = module.vnet-01.subnet_ids["subnet-lin"]
-  network_security_group_id = azurerm_network_security_group.ssh.id
-}
-#########################
 
-module "vm-lin-02" {
-  source = "git::https://github.com/Xaprun/tfmodules.git//modules/vm?ref=main"
-
-  resource_group_name     = azurerm_resource_group.rg-01.name
+module "vm_lin_02" {
+  source                  = "git::https://github.com/Xaprun/tfmodules.git//modules/vm?ref=main"
+  resource_group_name     = azurerm_resource_group.rg_01.name
   resource_group_location = var.location
-  # environment           = var.environment
-  network_name            = "${local.prefix}-vnet"
-
+  subnet_id               = module.vnet_01.subnet_ids["subnet-lin-prv"]
   admin_username          = "azureuser"
-  public_subnet_name      = keys(var.subnets)[2]
   admin_ssh_key_path      = "ssh/admin_key.pub"
-  custom_data_file      = "${path.root}/cloud-init/init.sh"
-  subnet_id               = module.vnet-01.subnet_ids["subnet-lin-prv"]
-  depends_on              = [azurerm_resource_group.rg-01, module.vnet-01]
+  # no custom_data, no public IP
+  enable_public_ip = false
 
   vm_config = {
     "${local.prefix}-vm-li02" = {
-      private_ip          = var.prv_ip_lin_prv
-      public_ip_name      = "vm-li02-public-ip"
       machine_type        = "Standard_B1ms"
-      machine_description = "Test VM 2"
+      machine_description = "private"
+      # assign_public_ip omitted -> false via module default
     }
   }
-  firewall_rules = {
-  }
 }
-
-# Attach NSG to the subnet your VM uses
-resource "azurerm_subnet_network_security_group_association" "subnet-lin-prv" {
-  subnet_id                 = module.vnet-01.subnet_ids["subnet-lin-prv"]
-  network_security_group_id = azurerm_network_security_group.ssh.id
-}
-
 
 
 
